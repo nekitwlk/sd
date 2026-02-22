fe3e087136737917aa9a71c48c5f909aec3c3d96
nekitwlk <mgrigorit@mail.ru>
2026-02-22 20:38:56 +0300

    Update next.config.mjs

--- backend/frontend/next.config.mjs
+++ backend/frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Полностью отключаем Turbopack
+  experimental: {
+    turbopack: false,
+  },
+};
 
 export default nextConfig;
